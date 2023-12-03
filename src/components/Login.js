826d7485545fc93a9f78a9087ac465bc2ece1553
NGunjyal01 <ngunjyal25@gmail.com>
2023-12-03 16:44:24 +0530

    add firebase and setup database for authentication

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,8 @@
 import { useState,useRef } from "react";
 import Header from "./Header";
 import {checkValidData} from "../utils/validate"
-
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import {auth} from "../utils/firebase"
 const Login = ()=>{
 
     const [isSignIn,setIsSignIn] = useState(true);
@@ -15,11 +16,39 @@ const Login = ()=>{
 
     const handleButtonClick = ()=>{
         //validate the form data
-        // console.log(email.current.value);
-        // console.log(password.current.value);
         const message = checkValidData(email.current.value,password.current.value);
-        // console.log(message);
         setErrorMessage(message);
+        if(message) return;
+        if(!isSignIn){
+            //Sign Up Logic
+            createUserWithEmailAndPassword(auth, email.current.value, password.current.value)
+            .then((userCredential) => {
+                // Signed up 
+                const user = userCredential.user;
+                console.log(user);
+                // ...
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                setErrorMessage(errorCode+"-"+errorMessage);
+                // ..
+            });
+        }
+        else{
+            signInWithEmailAndPassword(auth, email.current.value, password.current.value)
+            .then((userCredential) => {
+                // Signed in 
+                const user = userCredential.user;
+                console.log(user);
+                // ...
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                setErrorMessage(errorCode+"-"+errorMessage);
+            });
+        }
     };
 
     return (
---
+++ src/utils/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCV3C0TGqsFnk3KVRfkyXsMnPbPOSwu34c",
+  authDomain: "netflix-gpt-be012.firebaseapp.com",
+  projectId: "netflix-gpt-be012",
+  storageBucket: "netflix-gpt-be012.appspot.com",
+  messagingSenderId: "730124086076",
+  appId: "1:730124086076:web:988b1f0b23dd7842e5cc39",
+  measurementId: "G-0S1ENZBVY8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth = getAuth();
