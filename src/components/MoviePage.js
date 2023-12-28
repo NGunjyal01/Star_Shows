c6ee106b3d3ba9209151bd3b9db6d957fd2a163f
NGunjyal01 <ngunjyal25@gmail.com>
2023-12-29 00:20:32 +0530

    add Movie Page

---
+++ src/components/MoviePage.js
@@ -0,0 +1,15 @@
+
+
+const MoviePage = ({showMoviePage}) => {
+    return (
+        <div className="inset-0 bg-black bg-opacity-25 backdrop-blur-sm justify-center items-center">
+            <div className="w-1/2 bg-white text-black">
+                <div>
+                    hello
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MoviePage
