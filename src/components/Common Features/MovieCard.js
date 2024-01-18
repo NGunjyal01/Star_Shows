02fc85c34984cc103821cce05ee48b703ad69bd9
NGunjyal01 <ngunjyal25@gmail.com>
2024-01-18 12:30:35 +0530

    update genre routing and movies and tvshows list

--- src/components/Common Features/MovieCard.js
+++ src/components/Common Features/MovieCard.js
@@ -1,9 +1,9 @@
 import { IMG_CDN } from "../../utils/constants";
 
-const MovieCard = ({ posterPath}) => {
+const MovieCard = ({ posterPath }) => {
 
-    return !posterPath?null: (
-        <div className="relative w-28 md:w-40 px-1 sm:px-2 md:scale-90 md:hover:scale-[115%] transition-transform ease-in-out cursor-pointer">
+    return !posterPath?null: ( 
+        <div className="relative w-28 md:w-40 px-1 sm:px-2 md:scale-90 md:hover:scale-110 transition-transform ease-in-out cursor-pointer">
             <div>
                 <img alt="Movie Poster" src={IMG_CDN + posterPath} className="rounded-md sm:rounded-lg"/>
             </div>
--- src/components/Common Features/TVShowCard.js
+++ src/components/Common Features/TVShowCard.js
@@ -1,6 +1,6 @@
 import { IMG_CDN } from "../../utils/constants";
 
-const TVShowCard = ({ posterPath}) => {
+const TVShowCard = ({ posterPath }) => {
 
     return !posterPath?null: (
         <div className="relative w-28 md:w-40 px-1 sm:px-2 md:scale-90 md:hover:scale-[115%] transition-transform ease-in-out cursor-pointer">
