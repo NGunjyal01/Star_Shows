7fa5d82ff8afd7dcc204c1e5edc286d2ebe021d6
NGunjyal01 <ngunjyal25@gmail.com>
2024-01-29 23:10:22 +0530

    add react-hot-toast in add and more watchlist

--- src/components/Common Features/AddWatchlistIcon.js
+++ src/components/Common Features/AddWatchlistIcon.js
@@ -1,51 +1,62 @@
 import { useState } from "react";
-import { getFirestore,doc, updateDoc,collection,setDoc, arrayUnion } from "firebase/firestore";
+import { getFirestore,doc,updateDoc,arrayUnion,getDoc } from "firebase/firestore";
 import { useSelector } from "react-redux";
 import { useLocation } from "react-router-dom";
+import toast, { Toaster } from 'react-hot-toast';
 
 const AddWatchlistIcon = ({ id,poster_path }) => {
 
     const [hover, setHover] = useState(false);
+    const [movies,setMovies] = useState(null);
+    const [tvShows,setTVShows] = useState(null);
     const {pathname} = useLocation();
     const type = pathname.slice(1,8)==="tvshows"?"TVShows":"Movies";
     const user = useSelector((store) => store?.user?.uid);
+
+    const db = getFirestore();
+    const docRef = doc(db, "Users", user);
+    getDoc(docRef).then((docSnap)=>{
+        if (docSnap.exists()) {
+            setMovies(docSnap.data().movies);
+            setTVShows(docSnap.data().tvShows);
+        }
+    })
+
     const handleWatchlistButtonClick = async() => {
         if(!user)    return;
-        const db = getFirestore();
-        const docRef = doc(db, "Users", user);
         if(type==="Movies"){
-            await updateDoc(docRef, {
-                movies: arrayUnion({id:id,poster_path:poster_path})
-            });
+            if(!movies?.find(obj => obj.id===id)){
+                await updateDoc(docRef, {
+                    movies: arrayUnion({id:id,poster_path:poster_path})
+                });
+                return toast.success('Added To Your Watchlist');
+            }
+            else{
+                return toast.error("Already In Your Watchlist");
+            }
         }
         else{
-            await updateDoc(docRef, {
-                tvShows: arrayUnion({id:id,poster_path:poster_path}),
-            });
+            if(!tvShows?.find(obj => obj.id===id)){
+                await updateDoc(docRef, {
+                    tvShows: arrayUnion({id:id,poster_path:poster_path}),
+                });
+                return toast.success('Added To Your Watchlist');
+            }
+            else{
+                return toast.error("Already In Your Watchlist");
+            }
         }
-        console.log("done");
     };
 
     return (
     <>
-        <div
-        className="w-16 h-9 sm:h-14 rounded-lg bg-white bg-opacity-70 flex justify-center"
-        onMouseEnter={() => {
-            setHover(true);
-        }}
-        onMouseLeave={() => {
-            setHover(false);
-        }}
-        onClick={() => {
-            handleWatchlistButtonClick();
-        }}
-        >
-        {hover && (
-        <div className="absolute -mt-28 h-10 w-28 bg-white rounded-full flex justify-center items-center">
-            <div className="text-black font-semibold">Wishlist</div>
-        </div>
-        )}
-        <div className="text-4xl text-black sm:mt-1.5 -mt-1">+</div>
+        <div className="w-16 h-9 sm:h-14 rounded-lg bg-white bg-opacity-70 flex justify-center" 
+        onMouseEnter={() => {setHover(true);}} onMouseLeave={() => {setHover(false);}} onClick={handleWatchlistButtonClick}>
+            {hover && (<div className="absolute -mt-14 h-10 w-28 bg-white rounded-full flex justify-center items-center">
+                <div className="text-black font-semibold">Wishlist</div>
+            </div>)}
+            <div className="text-4xl text-black sm:mt-1.5 -mt-1">+</div>
+            <Toaster/>
         </div>
     </>
     );
--- src/components/Common Features/RemoveWatchlistIcon.js
+++ src/components/Common Features/RemoveWatchlistIcon.js
@@ -1,5 +1,5 @@
-import { useState } from "react";
 import { getFirestore,doc,updateDoc,arrayRemove } from "firebase/firestore";
+import toast, { Toaster } from "react-hot-toast";
 import { useSelector } from "react-redux";
 
 const RemoveWatchlistIcon = ({ id,poster_path,type }) => {
@@ -19,12 +19,13 @@ const RemoveWatchlistIcon = ({ id,poster_path,type }) => {
                 tvShows: arrayRemove({id:id,poster_path:poster_path}),
             });
         }
-        console.log("removed");
+        return toast.success('Successfully Removed From Watchlist');
     };
 
     return (
         <div className="w-[90%] sm:h-10 h-7 rounded-lg bg-white flex justify-center md:hover:scale-90 mt-2" onClick={handleWatchlistButtonClick}>
             <div className="text-4xl text-black -mt-2.5 sm:-mt-1">-</div>
+            <Toaster/>
         </div>
     );
 };
