1a6cbfc66dc865b7b85c57ebe71879ef37d54cef
NGunjyal01 <ngunjyal25@gmail.com>
2024-02-02 23:12:48 +0530

    some minor change

--- src/components/Genres/Genres.js
+++ src/components/Genres/Genres.js
@@ -45,10 +45,18 @@ const Genre = () => {
       <div className="fixed z-10 pt-[14.5%] md:pt-[4.7%] w-full">
         <div className={`flex flex-row pt-4 md:pt-1 pl-10 md:pl-20 ${isScrolled?'bg-[#141414]':''}`}>
           <h1 className="text-white mr-4 text-lg md:text-2xl">Genres :</h1>
-          <button className="bg-white flex w-32 sm:w-60 rounded-lg" onClick={handleOptionMenuClick}><h1 className="mx-auto my-auto text-xs sm:text-base">{option}</h1> <FaChevronDown className="DropDownIcon mr-1 my-2"/></button>
+          <button className="bg-white flex w-32 sm:w-60 rounded-lg" onClick={handleOptionMenuClick}>
+            <h1 className="mx-auto my-auto text-xs sm:text-base">{option}</h1> 
+            <FaChevronDown className="DropDownIcon mr-1 my-2"/>
+          </button>
         </div>
-        {showOptionMenu && <div className={`fixed flex flex-wrap mt-2 ml-[6.5rem] text-black bg-white max-w-[15rem] sm:max-w-xs rounded-lg  ${isScrolled?'bg-opacity-100':'bg-opacity-50'}`}>
-          {Genres.map(genre => <NavLink className={({isActive})=>`${isActive?'text-[#00ADB5]':""} m-1.5 md:hover:text-[#00ADB5] text-xs sm:text-base` } to={"/genre/"+genre.id} key={genre.id} onClick={handleOptionClick}>{genre.name}</NavLink>)}
+        {showOptionMenu && 
+        <div className={`fixed flex flex-wrap mt-2 ml-[6.5rem] text-black bg-white max-w-[15rem] sm:max-w-xs rounded-lg  ${isScrolled?'bg-opacity-100':'bg-opacity-50'}`}>
+          {Genres.map(genre => 
+            <NavLink className={({isActive})=>`${isActive?'text-[#00ADB5]':""} m-1.5 md:hover:text-[#00ADB5] text-xs sm:text-base` } 
+            to={"/genre/"+genre.id} key={genre.id} onClick={handleOptionClick}>
+              {genre.name}
+            </NavLink>)}
         </div>}
       </div>
       <Outlet/>
